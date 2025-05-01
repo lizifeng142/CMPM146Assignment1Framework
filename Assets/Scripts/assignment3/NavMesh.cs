53d11f2914caa74cf186d755beb88cfaea52f140
cMsLuigi <lrocha1@ucsc.edu>
2025-05-01 01:31:14 -0700

    Workingish NavMesh

--- Assets/Scripts/assignment3/NavMesh.cs
+++ Assets/Scripts/assignment3/NavMesh.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+///
 public class NavMesh : MonoBehaviour
 {
     public Graph MakeNavMesh(List<Wall> outline)
